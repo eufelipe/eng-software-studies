9004165ce4c2c0c592f73927ef7e20c0c81dfbbb
Felipe Rosas <contato@eufelipe.com>
2024-08-10 17:25:14 -0300

    chore: add workflow for deploy docs

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,52 +1,85 @@
-import starlight from '@astrojs/starlight';
-import { defineConfig } from 'astro/config';
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Guia de Estudos de Princípios e Padrões de Software',
-			social: {
-				github: 'https://github.com/eufelipe/eng-software-studies',
-			},
-			sidebar: [
-				{
-					label: 'Introdução',
-					items: [
-						{ label: 'Comece aqui', slug: 'introduction/why-design-patterns' },
-					],
-				},
-				{
-					label: 'Fundamentos de SOLID',
-					items: [
-						{ label: 'Por que SOLID?', slug: 'solid/why-solid' },
-						{ label: 'Princípio da Responsabilidade Única (SRP)', slug: 'solid/srp' },
-						{ label: 'Princípio Aberto/Fechado (OCP)', slug: 'solid/ocp' },
-						{ label: 'Princípio da Substituição de Liskov (LSP)', slug: 'solid/lsp' },
-						{ label: 'Princípio da Segregação de Interfaces (ISP)', slug: 'solid/isp' },
-						{ label: 'Princípio da Inversão de Dependência (DIP)', slug: 'solid/dip' },
-					],
-				},
-				{
-					label: 'Guia de Estudos de Design Patterns',
-					items: [
-						{ label: 'Por que Design Patterns?', slug: 'design-patterns/why-design-patterns' },
-						{ label: 'Padrões Criacionais', slug: 'design-patterns/creational' },
-						{ label: 'Padrões Estruturais', slug: 'design-patterns/structural' },
-						{ label: 'Padrões Comportamentais', slug: 'design-patterns/behavioral' },
-					],
-				},
-				{
-					label: 'Arquitetura de Software',
-					items: [
-						{ label: 'Por que Arquitetura de Software?', slug: 'architecture/why-architecture' },
-						{ label: 'Arquitetura em Camadas', slug: 'architecture/layered' },
-						{ label: 'Arquitetura Hexagonal', slug: 'architecture/hexagonal' },
-						{ label: 'Arquitetura Orientada a Eventos', slug: 'architecture/event-driven' },
-						{ label: 'Microservices', slug: 'architecture/microservices' },
-					],
-				},
-			],
-		}),
-	],
+  output: "static",
+  base: "/eng-software-studies/",
+  site: "https://eufelipe.github.io/eng-software-studies",
+  integrations: [
+    starlight({
+      title: "Guia de Estudos de Princípios e Padrões de Software",
+      social: {
+        github: "https://github.com/eufelipe/eng-software-studies",
+      },
+      sidebar: [
+        {
+          label: "Introdução",
+          items: [
+            { label: "Comece aqui", slug: "introduction/why-design-patterns" },
+          ],
+        },
+        {
+          label: "Fundamentos de SOLID",
+          items: [
+            { label: "Por que SOLID?", slug: "solid/why-solid" },
+            {
+              label: "Princípio da Responsabilidade Única (SRP)",
+              slug: "solid/srp",
+            },
+            { label: "Princípio Aberto/Fechado (OCP)", slug: "solid/ocp" },
+            {
+              label: "Princípio da Substituição de Liskov (LSP)",
+              slug: "solid/lsp",
+            },
+            {
+              label: "Princípio da Segregação de Interfaces (ISP)",
+              slug: "solid/isp",
+            },
+            {
+              label: "Princípio da Inversão de Dependência (DIP)",
+              slug: "solid/dip",
+            },
+          ],
+        },
+        {
+          label: "Guia de Estudos de Design Patterns",
+          items: [
+            {
+              label: "Por que Design Patterns?",
+              slug: "design-patterns/why-design-patterns",
+            },
+            {
+              label: "Padrões Criacionais",
+              slug: "design-patterns/creational",
+            },
+            {
+              label: "Padrões Estruturais",
+              slug: "design-patterns/structural",
+            },
+            {
+              label: "Padrões Comportamentais",
+              slug: "design-patterns/behavioral",
+            },
+          ],
+        },
+        {
+          label: "Arquitetura de Software",
+          items: [
+            {
+              label: "Por que Arquitetura de Software?",
+              slug: "architecture/why-architecture",
+            },
+            { label: "Arquitetura em Camadas", slug: "architecture/layered" },
+            { label: "Arquitetura Hexagonal", slug: "architecture/hexagonal" },
+            {
+              label: "Arquitetura Orientada a Eventos",
+              slug: "architecture/event-driven",
+            },
+            { label: "Microservices", slug: "architecture/microservices" },
+          ],
+        },
+      ],
+    }),
+  ],
 });
